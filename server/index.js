1672ff3546713cf9dba6f7ed3d74684ddd2fb6d8
SandyArdiyan <sanardiyan881@gmail.com>
2026-01-10 10:19:27 +0700

    Mengedit app use sensor

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const sensorRoutes = require('./routes/sensorRoutes');
+const authRoutes = require('./routes/authRoutes');
 
 const app = express();
 const PORT = 5000;
@@ -12,6 +13,8 @@ app.use(bodyParser.json());
 
 // Routes
 app.use('/api/sensor', sensorRoutes);
+app.use('/api/auth', authRoutes);     
+app.use('/api/sensor', sensorRoutes); 
 
 // Jalankan Server
 app.listen(PORT, () => {

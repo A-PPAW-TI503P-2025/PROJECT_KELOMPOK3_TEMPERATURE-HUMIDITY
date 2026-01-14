46a70178669068fa976a0553d1a2663ad76e64af
msidiq2235 <muhsidiq2005@gmail.com>
2026-01-14 22:22:33 +0700

    revisi interval waktu dari 3 ke 5 detik

--- client/src/App.js
+++ client/src/App.js
@@ -62,7 +62,7 @@ function App() {
       }
     };
     fetchData();
-    const interval = setInterval(fetchData, 3000);
+    const interval = setInterval(fetchData, 5000);
     return () => clearInterval(interval);
   }, [user]);
 
